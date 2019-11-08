c52cb3548e18e85bc83ae18041a494fcd4c887d7
vlad879 <vlad879999@gmai.com>
2019-11-08 16:26:57 +0300

    add dz2

---
+++ dz2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+  int *a;
+  int n;
+  int i, j, m, k = 0;
+
+  printf("Введите количество вершин: ");
+  scanf("%d", &n);
+
+  int g[n];
+  a = (int*)malloc(n*n * sizeof(int));
+
+srand(time(NULL));
+for (i = 0; i < n; ++i) { //цикл по строкам матрицы
+  for (j = 0+k; j < n; ++j) //цикл по столбцам матрицы
+    if (j != i) { 
+        *(a + i*n + j) = rand() % 2;
+        g[j] = *(a + i*n + j);
+    } else {
+        *(a + i*n + j) = 0;
+        g[j] = *(a + i*n + j);
+      }
+      ++k;
+  for (m = k-1; m < n; ++m)
+     *(a + m*n + (k-1)) = g[m];
+}
+
+// Вывод матрицы смежности
+  for (i = 0; i<n; i++) 
+  {
+    for (j = 0; j<n; j++)  
+      printf("%d ", *(a + i*n + j));
+      printf("\n");
+  }
+
+//Запись матрицы смежности в graph.dot для визуализации
+    FILE *file_ptr;
+    FILE* f = fopen("graph.dot", "w");
+    fprintf (f,"digraph test{\n");
+    for (i = 0; i<n; i++) 
+      for (j = 0; j<n; j++)  
+         if (*(a + i*n + j) > 0)
+                    fprintf(f,"%d->%d\n",i,j);
+          else fprintf(f,"%d\n",i);
+     
+    fprintf(f,"}");              
+    fclose(f);
+
+
+//Проверка графа на связность(по формуле)
+
+int reb = 0; //кол-во рёбер
+
+for (i = 0; i<n; i++) 
+    for (j = 0; j<n; j++)  
+        reb = reb + *(a + i*n + j);
+
+    k = 0.5*(n-1)*(n-2);
+    if (reb>k)
+        printf ("Граф связный\n");
+    if (reb<=k)
+        printf ("Граф не связный\n");
+
+printf("Матрица смежности записана в graph.dot для визуализации графа\n");
+
+free(a); 
+
+return 0;
+}
